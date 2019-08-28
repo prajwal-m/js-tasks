efd9857d4af682ff36e45e40e7d65e17fd10dada
prajwal-m <46987044+prajwal-m@users.noreply.github.com>
2019-08-29 00:18:46 +0530

    Add files via upload

---
+++ handlePost.js
@@ -0,0 +1,158 @@
+var queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+console.log(queryString);
+var queries = queryString.split("=");
+console.log(queries[1]);
+var postId = queries[1];
+console.log(localStorage.getItem(postId));
+var postData = JSON.parse(localStorage.getItem(postId));
+console.log(typeof postData);
+console.log(postData.titleData);
+
+var inputContainer = document.getElementById("input-data");
+
+function displayData(postData) {
+    var titleDiv = document.createElement("div");
+    titleDiv.className = "title-div";
+    inputContainer.appendChild(titleDiv);
+
+    var titlePara = document.createElement("p");
+    titlePara.innerHTML = postData.titleData + " -   ";
+    titlePara.className = "title-para";
+    titleDiv.appendChild(titlePara);
+
+    var authorPara = document.createElement("p");
+    authorPara.className = "author-para";
+    authorPara.innerHTML = " Posted by " + postData.authorData;
+    titleDiv.appendChild(authorPara);
+
+    var headerBreak = document.createElement("hr");
+    inputContainer.appendChild(headerBreak);
+
+    var imgData = document.createElement("img");
+    imgData.className = "image-data";
+    imgData.src = postData.imgURLData;
+    imgData.alt = "post";
+    inputContainer.appendChild(imgData);
+
+    var headerBreak1 = document.createElement("hr");
+    inputContainer.appendChild(headerBreak1);
+
+    var contentPara = document.createElement("p");
+    contentPara.className = "content-para";
+    contentPara.innerHTML = postData.contentData;
+    inputContainer.appendChild(contentPara);
+
+}
+
+// data display function call
+
+displayData(postData);
+
+// comment form toggle function
+
+function toggleComment() {
+    var commentForm = document.getElementById("comment-form");
+    var toggleButton = document.getElementById("comment-toggle");
+    commentForm.style.display = "block";
+    toggleButton.style.display = "none";
+}
+
+var commentArray = new Array();
+
+var replyArray = new Array();
+var container = document.getElementById("comment-thread");
+
+function createCommentDom() {}
+
+function submitComment() {
+    var dataObj = {};
+    var comment = document.getElementById("comment").value;
+
+    var commentPara = document.createElement("span");
+    commentPara.className = "comment-para";
+    commentPara.innerHTML = comment;
+    commentPara.id =
+        Math.random()
+        .toString(36)
+        .substring(2, 15) +
+        Math.random()
+        .toString(36)
+        .substring(2, 15);
+    container.appendChild(commentPara);
+    dataObj.id = commentPara.id;
+
+    dataObj.comment = comment;
+    dataObj.parentId = "";
+    commentArray.push(dataObj);
+    console.log(commentArray);
+
+    var replyIcon = document.createElement("i");
+    replyIcon.className = "fa fa-chevron-circle-right";
+    replyIcon.id = dataObj.id;
+    commentPara.appendChild(replyIcon);
+
+    replyIcon.addEventListener("click", function() {
+        console.log(this.id);
+        var replyToComment = prompt("Enter your reply");
+        var replyObj = {};
+        replyObj.id =
+            Math.random()
+            .toString(36)
+            .substring(2, 15) +
+            Math.random()
+            .toString(36)
+            .substring(2, 15);
+        replyObj.comment = replyToComment;
+        replyObj.parentId = this.id;
+
+        commentArray.push(replyObj);
+        replyArray.push(replyObj);
+        console.log(commentArray);
+        console.log(replyArray);
+
+        commentReply(replyObj);
+    });
+}
+
+function commentReply(replyObj) {
+    console.log(replyObj);
+
+    var parentContainer = document.getElementById(replyObj.parentId);
+    console.log(parentContainer);
+    var replyComment = document.createElement("p");
+    replyComment.className = "comment-reply";
+    replyComment.id = replyObj.id;
+    replyComment.innerHTML = replyObj.comment;
+    parentContainer.appendChild(replyComment);
+
+    var replyIcon = document.createElement("i");
+    replyIcon.className = "fa fa-chevron-circle-right";
+    replyIcon.id = replyObj.id;
+    replyComment.appendChild(replyIcon);
+
+    console.log(commentArray);
+    console.log(replyArray);
+
+    replyIcon.addEventListener("click", function() {
+        console.log(this.id);
+        var replyToCmt = prompt("Enter your reply");
+        var replyObject = {};
+        replyObject.id =
+            Math.random()
+            .toString(36)
+            .substring(2, 15) +
+            Math.random()
+            .toString(36)
+            .substring(2, 15);
+        replyObject.comment = replyToCmt;
+        replyObject.parentId = this.id;
+
+        commentArray.push(replyObject);
+        replyArray.push(replyObject);
+        console.log(commentArray);
+        console.log(replyArray);
+
+        commentReply(replyObject);
+    });
+}
---
+++ handleReddit.js
@@ -0,0 +1,149 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+var dataArray = new Array();
+
+function moveLocation(id) {
+    console.log("moved");
+    console.log(id);
+    var queryString = "?value=" + id;
+    window.location.href = "postPage.html" + queryString;
+    console.log("moved to content");
+}
+
+function createDomElements(dataObject) {
+    var container = document.getElementById("input-container");
+    console.log("inside create dom method");
+    console.log(container);
+    var newDiv = document.createElement("div");
+    newDiv.className = "childDiv";
+    newDiv.id =
+        Math.random()
+        .toString(36)
+        .substring(2, 15) +
+        Math.random()
+        .toString(36)
+        .substring(2, 15);
+    container.appendChild(newDiv);
+    newDiv.addEventListener("click", function() {
+        console.log("clicked");
+        console.log(this.id);
+        moveLocation(this.id);
+    });
+
+    localStorage.setItem(newDiv.id, JSON.stringify(dataObject));
+
+    var titleElement = document.createElement("p");
+    titleElement.innerHTML = dataObject.titleData + " ";
+    titleElement.className = "title-element";
+    newDiv.appendChild(titleElement);
+
+    var authorElement = document.createElement("p");
+    authorElement.innerHTML = " -Posted by  " + dataObject.authorData;
+    authorElement.className = "author-element";
+    titleElement.appendChild(authorElement);
+
+    var imgElement = document.createElement("img");
+    imgElement.src = dataObject.imgURLData;
+    imgElement.className = "image-element";
+    //   newDiv.appendChild(imgElement)
+
+    dataObject.imgURLData != "" ?
+        (newDiv.appendChild(imgElement), (newDiv.style.marginLeft = "150px")) :
+        ((imgElement.style.display = "none"),
+            (newDiv.style.height = "250px"),
+            (newDiv.style.marginLeft = "150px"));
+
+    var contentElement = document.createElement("p");
+    contentElement.innerHTML = dataObject.contentData;
+    contentElement.className = "content-element";
+    newDiv.appendChild(contentElement);
+
+    var iconDiv = document.createElement("div");
+    iconDiv.className = "icon-div";
+    newDiv.appendChild(iconDiv);
+
+    var commentIcon = document.createElement("i");
+    commentIcon.className = "fa fa-comments";
+    iconDiv.appendChild(commentIcon);
+
+    var shareIcon = document.createElement("i");
+    shareIcon.className = "fa fa-share";
+    iconDiv.appendChild(shareIcon);
+
+    var upvoteIcon = document.createElement("i");
+    upvoteIcon.className = "fa fa-arrow-up";
+    iconDiv.appendChild(upvoteIcon);
+
+    var voteCount = document.createElement("p");
+    voteCount.className = "vote-count";
+    voteCount.id =
+        Math.random()
+        .toString(36)
+        .substring(2, 15) +
+        Math.random()
+        .toString(36)
+        .substring(2, 15);
+    voteCount.innerHTML = "";
+    iconDiv.appendChild(voteCount);
+
+    var downvoteIcon = document.createElement("i");
+    downvoteIcon.className = "fa fa-arrow-down";
+    iconDiv.appendChild(downvoteIcon);
+
+    var count = 0;
+    upvoteIcon.addEventListener("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var incrCount = document.getElementById(voteCount.id);
+        incrCount.innerHTML = ++count;
+    });
+
+    downvoteIcon.addEventListener("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var decrCount = document.getElementById(voteCount.id);
+        count > 0 ? (decrCount.innerHTML = --count) : (decrCount.innerHTML = "");
+    });
+}
+
+// handling form submit
+
+function handleSubmit() {
+    var title = document.getElementById("form-title").value;
+    var author = document.getElementById("form-author").value;
+    var content = document.getElementById("form-content").value;
+    var imgURL = document.getElementById("form-image").value;
+    var container = document.getElementById("input-container");
+    var dataObject = {
+        titleData: title,
+        authorData: author,
+        contentData: content,
+        imgURLData: imgURL
+    };
+
+    dataArray.push(dataObject);
+    console.log(dataArray);
+
+    console.log(title);
+    console.log(author);
+    console.log(content);
+    console.log(imgURL);
+    closeForm();
+
+    createDomElements(dataObject);
+}
+
+var staticData = {
+    titleData: "Animals",
+    authorData: "Martin Garrix",
+    contentData: "Martijn Gerard Garritsen, professionally known as Martin Garrix, is a Dutch DJ and record producer from Amstelveen. His most known singles are . He was ranked number one on DJ Mag's Top 100 DJs list for three consecutive years",
+    imgURLData: "https://yt3.ggpht.com/a/AGF-l78q93xLts3A_ZL0ZetQ4JpMdeOxq624ENl3tg=s900-c-k-c0xffffffff-no-rj-mo"
+};
+
+createDomElements(staticData);
